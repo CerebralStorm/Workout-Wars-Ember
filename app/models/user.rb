fa529e991cc82c9e54ab1bbd93f4c1e8df9d2d9f
Cody Tanner <cerebralstorm@gmail.com>
2014-03-04 23:31:08 -0700

    attempting to get guest png working for gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,13 +17,13 @@ class User < ActiveRecord::Base
   before_save :set_avatar_url
 
   def set_avatar_url
-    if avatar_url.present?
-      avatar_url
-    else
-      default_url = "#{Rails.root}images/guest.png"
+    # if avatar_url.present?
+    #   avatar_url
+    # else
+      default_url = "#{Rails.root}/assets/guest.png"
       gravatar_id = Digest::MD5.hexdigest(email.downcase)
-      self.avatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-    end
+      self.avatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{Rack::Utils.escape(default_url)}"
+    #end
   end
   
   def create_competition_activities(activity)
