8f38d9c9239696edd08721e7f2ca01d3d4f34dd5
Cody Tanner <cerebralstorm@gmail.com>
2014-03-04 18:29:43 -0700

    update schema to mark existing exercises as approved

--- db/migrate/20140225163207_add_user_id_and_flag_to_exercise.rb
+++ db/migrate/20140225163207_add_user_id_and_flag_to_exercise.rb
@@ -1,4 +1,24 @@
 class AddUserIdAndFlagToExercise < ActiveRecord::Migration
+
+[
+  {name: 'Pushups'  }   
+  {name: 'Pullups'  }   
+  {name: 'Situps'   }    
+  {name: 'Squats'   }    
+  {name: 'Lunges'   }    
+  {name: 'Burpees'  }   
+  {name: 'Running'  }   
+  {name: 'Biking'   }    
+  {name: 'Swimming' }  
+  {name: 'Hiking'   }    
+  {name: 'Walking'  }   
+  {name: 'Aerobics' }  
+  {name: 'Zumba'    }     
+  {name: 'Yoga'     }      
+].each do |exercise|
+  Exercise.find_by(name: exercise).update_attributes(approved: true)
+end
+
   def change
     add_column :exercises, :approved, :boolean
     add_column :challenge_attempts, :activity_id, :integer
