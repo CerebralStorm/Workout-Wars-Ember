79e892e12dbaee1d4f29d2426ab30181c7b1f50e
Cody Tanner <cerebralstorm@gmail.com>
2014-03-04 18:26:35 -0700

    cleans up migration

--- db/migrate/20140225163207_add_user_id_and_flag_to_exercise.rb
+++ db/migrate/20140225163207_add_user_id_and_flag_to_exercise.rb
@@ -1,7 +1,9 @@
 class AddUserIdAndFlagToExercise < ActiveRecord::Migration
   def change
-    add_column :exercises, :user_id, :integer
     add_column :exercises, :approved, :boolean
     add_column :challenge_attempts, :activity_id, :integer
+    add_index :challenge_attempts, [:user_id, :challenge_id]
+    drop_table :exercise_sets
+    drop_table :user_exercises
   end
 end
