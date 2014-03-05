7e4b9a7ece7e15b09698a6ee8696eddf5a6a7611
Cody Tanner <cerebralstorm@gmail.com>
2014-03-04 18:32:10 -0700

    adds missing commas

--- db/migrate/20140225163207_add_user_id_and_flag_to_exercise.rb
+++ db/migrate/20140225163207_add_user_id_and_flag_to_exercise.rb
@@ -1,19 +1,19 @@
 class AddUserIdAndFlagToExercise < ActiveRecord::Migration
 
 [
-  {name: 'Pushups'  }   
-  {name: 'Pullups'  }   
-  {name: 'Situps'   }    
-  {name: 'Squats'   }    
-  {name: 'Lunges'   }    
-  {name: 'Burpees'  }   
-  {name: 'Running'  }   
-  {name: 'Biking'   }    
-  {name: 'Swimming' }  
-  {name: 'Hiking'   }    
-  {name: 'Walking'  }   
-  {name: 'Aerobics' }  
-  {name: 'Zumba'    }     
+  {name: 'Pushups'  },   
+  {name: 'Pullups'  },   
+  {name: 'Situps'   },    
+  {name: 'Squats'   },    
+  {name: 'Lunges'   },    
+  {name: 'Burpees'  },   
+  {name: 'Running'  },   
+  {name: 'Biking'   },    
+  {name: 'Swimming' },  
+  {name: 'Hiking'   },    
+  {name: 'Walking'  },   
+  {name: 'Aerobics' },  
+  {name: 'Zumba'    },     
   {name: 'Yoga'     }      
 ].each do |exercise|
   Exercise.find_by(name: exercise).update_attributes(approved: true)
