5973b01834c2886e95b7cff5e7908087df262231
Cody Tanner <cerebralstorm@gmail.com>
2014-03-31 22:32:53 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,23 +16,6 @@ ActiveRecord::Schema.define(version: 20140327182322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: true do |t|
-    t.integer  "trackable_id"
-    t.string   "trackable_type"
-    t.integer  "owner_id"
-    t.string   "owner_type"
-    t.string   "key"
-    t.text     "parameters"
-    t.integer  "recipient_id"
-    t.string   "recipient_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
-
   create_table "challenge_attempts", force: true do |t|
     t.integer  "user_id"
     t.integer  "challenge_id"
@@ -114,16 +97,6 @@ ActiveRecord::Schema.define(version: 20140327182322) do
     t.datetime "updated_at"
   end
 
-  create_table "exercise_metrics", force: true do |t|
-    t.integer  "exercise_id"
-    t.integer  "metric_id"
-    t.float    "experience_multiplier"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "exercise_metrics", ["exercise_id", "metric_id"], name: "index_exercise_metrics_on_exercise_id_and_metric_id", using: :btree
-
   create_table "exercises", force: true do |t|
     t.string   "name"
     t.float    "experience_multiplier"
@@ -181,16 +154,6 @@ ActiveRecord::Schema.define(version: 20140327182322) do
   end
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
-
-  create_table "user_exercise_values", force: true do |t|
-    t.integer  "metric_id"
-    t.integer  "user_exercise_id"
-    t.float    "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "user_exercise_values", ["user_exercise_id", "metric_id"], name: "index_user_exercise_values_on_user_exercise_id_and_metric_id", using: :btree
 
   create_table "user_exercises", force: true do |t|
     t.integer  "exercise_id"
