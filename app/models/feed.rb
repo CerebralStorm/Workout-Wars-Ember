965735aa0fba0b17fca6663a04c5703cf381a4ad
Cody Tanner <cerebralstorm@gmail.com>
2014-04-05 17:01:02 -0600

    adds spec tests for feeds

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,5 @@
 class Feed < ActiveRecord::Base
   belongs_to :feedable, polymorphic: true
+
+  validates_presence_of :feedable
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Feed do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "associations" do
+    it 'should have many user exercises' do
+      Feed.reflect_on_association(:feedable).should_not be_nil
+      Feed.reflect_on_association(:feedable).macro.should eql(:belongs_to)
+    end
+  end
+
+  context "validations" do
+    it "should require an email" do
+      FactoryGirl.build(:feed, feedable: nil).should_not be_valid
+    end
+
+    it "should be valid with a user and competition" do
+      exercise = FactoryGirl.build(:user_exercise)
+      FactoryGirl.build(:feed, feedable: exercise).should be_valid
+    end
+  end
 end
