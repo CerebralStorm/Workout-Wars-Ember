a609e8c868b6ef70f853a5c54abc8907e1043fc5
Cody Tanner <cerebralstorm@gmail.com>
2014-04-15 17:40:15 -0600

    update zero push initializer

--- config/initializers/zero_push.rb
+++ config/initializers/zero_push.rb
@@ -1,7 +1,5 @@
-# if Rails.env.production?
-#   ZeroPush.auth_token = ENV["ZEROPUSH_PROD_TOKEN"]
-# else
-#   ZeroPush.auth_token = ENV["ZEROPUSH_DEV_TOKEN"]
-# end
-
-ZeroPush.auth_token = ENV["ZEROPUSH_DEV_TOKEN"]
+if Rails.env.production?
+  ZeroPush.auth_token = ENV["ZEROPUSH_PROD_TOKEN"]
+else
+  ZeroPush.auth_token = ENV["ZEROPUSH_DEV_TOKEN"]
+end
